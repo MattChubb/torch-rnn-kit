f0551afaf274c631770a2d51833fc66beff6a531
Matthew Chubb <matthew.chubb@zpg.co.uk>
2019-03-20 17:26:12 +0000

    Initial commit

---
+++ preprocess.sh
@@ -0,0 +1,1 @@
+docker run --rm -v ${PWD}:/data -ti crisbal/torch-rnn:base /data/scripts/preprocess.sh
---
+++ scripts/preprocess.sh
@@ -0,0 +1,4 @@
+python scripts/preprocess.py \
+ --input_txt /data/source_data/* \
+ --output_h5 data/processed_data/data.h5  \
+ --output_json data/processed_data/data.json
