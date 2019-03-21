f2f2df3bab691c65d939139e067bf7ac111a301e
Matthew Chubb <matthew.chubb@zpg.co.uk>
2019-03-21 09:52:40 +0000

    Preprocess script

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+
+cat /data/source_data/* > /data/source_data.all
+
 python scripts/preprocess.py \
- --input_txt /data/source_data/* \
- --output_h5 data/processed_data/data.h5  \
- --output_json data/processed_data/data.json
+	--input_txt /data/source_data.all \
+	--output_h5 /data/processed_data/data.h5 \
+	--output_json /data/processed_data/data.json
+
+rm /data/source_data.all
