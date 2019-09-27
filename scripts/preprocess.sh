6921a03d01401e3f21a58156d09f8b6c7acb0302
Matt Chubb <chubb.matthew@gmail.com>
2019-09-27 17:14:20 +0100

    Allow passing params through to all scripts

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -5,6 +5,7 @@ cat /data/source_data/* > /data/source_data.all
 python scripts/preprocess.py \
 	--input_txt /data/source_data.all \
 	--output_h5 /data/processed_data/data.h5 \
-	--output_json /data/processed_data/data.json
+	--output_json /data/processed_data/data.json \
+    $@
 
 rm /data/source_data.all
--- scripts/train.sh
+++ scripts/train.sh
@@ -2,7 +2,7 @@
 echo The training process is very CPU intensive and can take a long time, but checkpoints are made every 200 iterations.
 echo If the script is stopped before completion \(Ctrl+C\), sampling will make use of the most recent checkpoint.
 
-th train.lua -input_h5 /data/processed_data/data.h5 -input_json /data/processed_data/data.json -init_from /data/models/model.t7  -gpu -1 -checkpoint_every 200 -rnn_size 512 -num_layers 3 -dropout 0.1 -checkpoint_name /tmp/checkpoint_512_3
+th train.lua -input_h5 /data/processed_data/data.h5 -input_json /data/processed_data/data.json -init_from /data/models/model.t7  -gpu -1 -checkpoint_every 200 -rnn_size 512 -num_layers 3 -dropout 0.1 -checkpoint_name /tmp/checkpoint_512_3 $@
 
 cp `ls -tr /tmp/checkpoint_512_3*.json | tail -1` /data/models/model.json
 cp `ls -tr /tmp/checkpoint_512_3*.t7 | tail -1` /data/models/model.t7
