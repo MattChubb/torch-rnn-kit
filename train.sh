518d08124c94e3b794fb70c251aae45dd05077a9
Matthew Chubb <chubb.matthew@gmail.com>
2019-10-30 21:24:55 +0000

    Remove single CPU default

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-docker run --cpus 1 --rm -v ${PWD}:/data -ti crisbal/torch-rnn:base /data/scripts/train.sh "$@"
+docker run --rm -v ${PWD}:/data -ti crisbal/torch-rnn:base /data/scripts/train.sh "$@"
