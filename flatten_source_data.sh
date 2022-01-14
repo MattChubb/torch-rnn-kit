07c72aac3bf0868211aaea869caf5c8e8dc9d312
Matt Chubb <matt.chubb@itech.media>
2022-01-14 10:16:31 +0000

    Filter out binary files when flattening

--- flatten_source_data.sh
+++ flatten_source_data.sh
@@ -1,1 +1,1 @@
-for DEPTH in {1..10}; do find ./source_data -name "*" -mindepth $DEPTH -exec mv {} ./source_data \;; done
+for DEPTH in {1..10}; do find ./source_data -name "*" -mindepth $DEPTH -exec grep -Iq . {} \; -exec mv {} ./source_data \;; done
